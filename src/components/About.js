08dc24690eb3a30a707127cc5996483d5a523a03
Jason A. Arter <jason.arter@gmail.com>
2023-03-30 22:08:51 -0700

    Updated profile picture.

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className="card mx-auto" style={{ maxWidth: "50vw" }}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={require('../images/Jason1.jpg')} className="img-fluid rounded-start" alt="The author." />
+                    <img src={require('../images/profile_pic.jpg')} className="img-fluid rounded-start" alt="The author." />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
@@ -16,13 +16,14 @@ export default function About() {
                             After the Army, I used the G.I. Bill to attend college for a degree in Electrical Engineering.
                         </p>
                         <p className="card-text">
-                            I spent the next 23 years at a wireless carrier where I worked as a Field Technician, a Field Supervisor, and finally a Switching Technician.<br></br>
-                            While there, I learned about database management, machine learning, Linux system administration, VBA, Python, Perl, and Django.
+                            I spent the next 23 years at a wireless carrier where I worked as a Field Technician, a Field Supervisor, and finally a Switching Technician.
+                            While there, I learned about database management, machine learning, Linux system administration, VBA, Python, and Django.
                         </p>
                         <p className="card-text">
-                            At some point, I realized I enjoyed the programming part of the job more than anything else. This 
+                            At some point, I realized I enjoyed the programming part of the job more than anything else. This love for code ultimately led to me
+                            enrolling in a Full-Stack development boot camp through the University of Washington which I completed in March of 2023.
                         </p>
-                        <p className="card-text"><small className="text-muted">Last updated 2023-03-30</small></p>
+                        <p className="card-text"><small className="text-muted">Last updated 2023-03-31</small></p>
                     </div>
                 </div>
             </div>
